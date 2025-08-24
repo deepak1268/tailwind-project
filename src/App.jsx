ad4202f183f561bbebdbc14b9ae8380e8153520d
Deepak Aggarwal <deepakagg1206@gmail.com>
2025-08-25 02:00:06 +0530

    Fixed Out of Screen Issue

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,11 @@ function MainContent() {
       <div className="translate-x-86 mt-10 ">
         <Greeting></Greeting>
       </div>
-      <div className="ml-7 w-full grid grid-cols-13 gap-7">
-        <div className="col-span-3 bg-[#fffffe] h-90 rounded-2xl shadow-lg -translate-y-40">
+      <div className="w-full grid grid-cols-12 gap-7">
+        <div className="ml-5 col-span-3 bg-[#fffffe] h-90 rounded-2xl shadow-lg -translate-y-40">
           <Profile></Profile>
         </div>
-        <div className="col-span-6 bg-[#fffffe] h-120 rounded-2xl shadow-lg p-10 translate-y-9">
+        <div className="col-span-5 bg-[#fffffe] h-120 rounded-2xl shadow-lg p-10 translate-y-9">
           <Schedule></Schedule>
           <WebinarCard time={"11:30 AM"} status={true} title={"UX Webinar"}></WebinarCard>
           <div className="h-1 bg-gray-200"></div>
